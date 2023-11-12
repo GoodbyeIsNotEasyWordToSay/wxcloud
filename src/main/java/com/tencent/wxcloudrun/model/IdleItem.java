7744f6938dd3dd8826f3c4fe01795acb6ee7db44
Jiaqi <2585009033@qq.com>
2023-11-13 01:09:43 +0800

    为两个good的子类添加注释@EqualsAndHashCode(callSuper = true)

--- src/main/java/com/tencent/wxcloudrun/model/IdleItem.java
+++ src/main/java/com/tencent/wxcloudrun/model/IdleItem.java
@@ -3,9 +3,9 @@ package com.tencent.wxcloudrun.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class IdleItem extends Good {
     protected LocalDateTime endtime;
