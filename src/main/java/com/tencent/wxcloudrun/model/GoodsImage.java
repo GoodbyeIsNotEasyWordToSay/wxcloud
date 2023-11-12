8423b3854d467ed8bd857dd227d28f37a761ba4b
Jiaqi <2585009033@qq.com>
2023-11-13 02:51:48 +0800

    debug: implements Serializable

--- src/main/java/com/tencent/wxcloudrun/model/GoodsImage.java
+++ src/main/java/com/tencent/wxcloudrun/model/GoodsImage.java
@@ -2,8 +2,10 @@ package com.tencent.wxcloudrun.model;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class GoodsImage {
+public class GoodsImage implements Serializable {
     protected Integer GImageID;
     protected Integer GID;
     protected String IURL;
