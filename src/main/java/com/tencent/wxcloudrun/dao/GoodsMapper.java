90addc08e3f859a1f274d84c839170334df42a66
Jiaqi <2585009033@qq.com>
2023-11-12 14:26:01 +0800

    测试mapper注解实现

--- src/main/java/com/tencent/wxcloudrun/dao/GoodsMapper.java
+++ src/main/java/com/tencent/wxcloudrun/dao/GoodsMapper.java
@@ -3,8 +3,10 @@ package com.tencent.wxcloudrun.dao;
 import com.tencent.wxcloudrun.model.Goods;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface GoodsMapper {
+    @Select("select * from goods where GID = #{GID}")
     Goods getGoods(@Param("GID") int gid);
 }
