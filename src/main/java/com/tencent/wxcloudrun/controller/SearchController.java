bef31b0631f5da68341f059297454be0dbdb37ad
Strive236 <1595709321@qq.com>
2023-11-24 16:40:49 +0800

    搜索方法Get改Post——搜索引擎更改请求

--- src/main/java/com/tencent/wxcloudrun/controller/SearchController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/SearchController.java
@@ -1,12 +1,11 @@
 package com.tencent.wxcloudrun.controller;
 
+import com.tencent.wxcloudrun.dto.SearchRequest;
 import com.tencent.wxcloudrun.model.Good;
 import com.tencent.wxcloudrun.service.SearchService;
 import com.tencent.wxcloudrun.config.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Optional;
@@ -17,8 +16,9 @@ public class SearchController {
 
     public SearchController(@Autowired SearchService searchService) { this.searchService = searchService; }
 
-    @GetMapping("/api/goods/search/{Gdes}")
-    public ApiResponse searchGoods(@PathVariable String Gdes) {
+    @PostMapping("/api/goods/search")
+    public ApiResponse searchGoods(@RequestBody SearchRequest searchRequest) {
+        String Gdes = searchRequest.getKeyword();
         Optional<ArrayList<Good>> goodsList = searchService.searchGoods(Gdes);
         if (goodsList.isPresent()) {
             return ApiResponse.ok(goodsList); // 如果成功获取到商品，则返回成功的响应
