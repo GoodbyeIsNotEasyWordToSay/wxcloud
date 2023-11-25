2553ca8140db63510429dece1e2a6855b36e2af9
Jiaqi <2585009033@qq.com>
2023-11-26 01:21:46 +0800

    创建了登录（注册）接口以及获取openid的接口。

---
+++ src/main/java/com/tencent/wxcloudrun/dao/UserMapper.java
@@ -0,0 +1,18 @@
+package com.tencent.wxcloudrun.dao;
+
+import com.tencent.wxcloudrun.model.User;
+import com.tencent.wxcloudrun.model.UserImage;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+public interface UserMapper {
+    @Select("select UID from user where UID = #{uid}")
+    String getUID(@Param("uid") String openid);
+
+    @Insert("insert into user (UID, Urole, Uname) values (#{uid}, #{role}, #{name})")
+    void createUser(User user);
+
+    @Insert("insert into user_image (u_id, i_url, i_category) values (#{u_id}, #{i_url}, #{i_category})")
+    void insertUserProfilePhoto(UserImage userImage);
+}
